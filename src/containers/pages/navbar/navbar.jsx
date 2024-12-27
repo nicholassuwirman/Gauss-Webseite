c4fef734435796ce336172d5844b76aca5fcb21e
Nicholas <wilsonsuwirmans@gmail.com>
2024-12-27 21:46:40 +0100

    navbar bug vercel

--- src/containers/pages/navbar/navbar.jsx
+++ src/containers/pages/navbar/navbar.jsx
@@ -9,14 +9,17 @@ import gauss1Mobile from '../../../assets/gauss-1-mobile.jpg'; // Update path as
 import gauss2Mobile from '../../../assets/gauss-2-mobile.jpg'; // Update path as necessary
 
 const Menu = () => {
+  const handleScrollToTop = () => {
+    window.scrollTo(0, 0); // Scroll to top
+  };
+
   return (
     <>
-      <Link to="/bikesHome" className="navbar-menu-text">Bikes</Link>
-      <Link to="/about" className="navbar-menu-text">About</Link>
-      <Link to="/" className="navbar-menu-text">Partners</Link>
-      <Link to="/" className="navbar-menu-text">Department</Link>
-
-      <Link to="/" className="navbar-menu-text">Participate</Link>
+      <Link to="/bikesHome" className="navbar-menu-text" onClick={handleScrollToTop}>Bikes</Link>
+      <Link to="/about" className="navbar-menu-text" onClick={handleScrollToTop}>About</Link>
+      <Link to="/" className="navbar-menu-text" onClick={handleScrollToTop}>Partners</Link>
+      <Link to="/" className="navbar-menu-text" onClick={handleScrollToTop}>Department</Link>
+      <Link to="/" className="navbar-menu-text" onClick={handleScrollToTop}>Participate</Link>
     </>
   );
 };
@@ -57,10 +60,14 @@ const Navbar = () => {
     };
   }, [toggleMenu]);
 
+  const handleScrollToTop = () => {
+    window.scrollTo(0, 0); // Scroll to top when menu is toggled
+  };
+
   return (
     <div>
       <div className={`navbar-section ${scrolled ? 'scrolled' : ''}`}>
-        <Link to="/">
+        <Link to="/" onClick={handleScrollToTop}>
           <img src={gaussLogoRed} alt="Gauss Logo" className="logo" />
         </Link>
 
@@ -77,8 +84,6 @@ const Navbar = () => {
             <RiMenuLine color="#FFFFFF" size={27} onClick={handleMenuToggle} className="open-logo" />
           )}
         </div>
-
-        
       </div>
 
       <div className="navbar-white-line hidden sm:block ml-20 mt-[11px] w-[91%] border-b-2 border-[#F9F7F7] h-[4px] relative"></div>
@@ -87,22 +92,20 @@ const Navbar = () => {
       <div className={`navbar-mobile-container ${toggleMenu ? 'visible' : ''}`}>
         <div className="navbar-mobile-content">
           <div className="navbar-menu-bikes-mobile">
-            <Link to="/bikesHome" className="navbar-menu-text-bikes">Bikes</Link>
+            <Link to="/bikesHome" className="navbar-menu-text-bikes" onClick={handleScrollToTop}>Bikes</Link>
             <div className="bikes-picture-container flex flex-row">
-              <Link to="/gauss1" className="bikes-link">  {/* Add the path where you want to navigate */}
+              <Link to="/gauss1" className="bikes-link" onClick={handleScrollToTop}>
                 <img src={gauss1Mobile} alt="Gauss 1 Image" className="gauss-bikes-mobile" />
               </Link>              
-              <Link to="/gauss2" className="bikes-link">  {/* Add the path where you want to navigate */}
+              <Link to="/gauss2" className="bikes-link" onClick={handleScrollToTop}>
                 <img src={gauss2Mobile} alt="Gauss 2 Image" className="gauss-bikes-mobile" />
               </Link>  
             </div>
           </div>
-          <Link to="/about" className="navbar-menu-text-about">About</Link>
-
-          <Link to="/" className="navbar-menu-text-partners">Partners</Link>
-          <Link to="/" className="navbar-menu-text-partners">Department</Link>
-
-          <Link to="/" className="navbar-menu-text-participate">Participate</Link>
+          <Link to="/about" className="navbar-menu-text-about" onClick={handleScrollToTop}>About</Link>
+          <Link to="/" className="navbar-menu-text-partners" onClick={handleScrollToTop}>Partners</Link>
+          <Link to="/" className="navbar-menu-text-partners" onClick={handleScrollToTop}>Department</Link>
+          <Link to="/" className="navbar-menu-text-participate" onClick={handleScrollToTop}>Participate</Link>
         </div>
       </div>
     </div>
